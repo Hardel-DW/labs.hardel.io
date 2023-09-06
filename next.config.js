321c6f6088ce302c9fca1e6ebc75337f1953376c
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2023-09-06 18:35:17 +0200

    Added Images domains in NextJS

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    images: {
+        domains: ['hardel.io', 'avatars.githubusercontent.com'],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**.hardel.io'
+            }
+        ]
+    }
 };
 
 module.exports = nextConfig;
