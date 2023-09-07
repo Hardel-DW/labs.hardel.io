0aed6ec1e15eef1b284ed450dfa06e143392ef8f
Hardel <42413417+Hardel-DW@users.noreply.github.com>
2023-09-08 00:29:39 +0200

    Add post method for item icon upload

---
+++ src/app/api/projects/[identifier]/items/icons/route.ts
@@ -0,0 +1,41 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { RestErrorHandler } from '@/libs/rest-error-handler';
+import { ErrorType, StatusCode } from '@/libs/constant';
+import RestUsers from '@/libs/rest-users';
+import ProjectRepository from '@repositories/Project';
+import prisma from '@/libs/prisma';
+import uploadAsset from '@/libs/aws/upload';
+import ItemRepository from '@repositories/Items';
+
+type Params = {
+    params: {
+        identifier: string;
+    };
+};
+
+export async function POST(request: NextRequest, { params }: Params) {
+    const data = await request.formData();
+    const files = data.get('images') as File | null;
+    const itemId = data.get('fileName') as string | null;
+    const destination = `project/${params.identifier}/item`;
+
+    if (!files || !itemId) {
+        return NextResponse.json(new RestErrorHandler(ErrorType.BadRequest, 'No icons provided'), { status: StatusCode.BadRequest });
+    }
+
+    try {
+        const userId = await new RestUsers(request).getUser();
+        await new ProjectRepository(prisma.project).checkIfUserIsInProject(params.identifier, userId);
+        await new ItemRepository(prisma.item).checkIfItemExists(params.identifier, itemId);
+
+        const response = await uploadAsset(destination, files, {
+            height: 64,
+            width: 64,
+            filename: itemId
+        });
+
+        return NextResponse.json(response);
+    } catch (error: any) {
+        return NextResponse.json(new RestErrorHandler(ErrorType.InternalServerError, error), { status: StatusCode.InternalServerError });
+    }
+}
